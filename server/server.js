ba36dcba02844ab71c3c6397493399ff4f691b63
Umair <umairtechie1@gmail.com>
2018-07-21 23:02:18 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
+var app = express();
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Connection established on port ${port}`);
+});
